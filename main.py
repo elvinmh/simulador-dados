0dbc4183ee1fbf253b28f81ca51d741f86ef55bb
Elvin Medina <elvinmedina@learning.eoi.es>
2025-05-31 19:20:32 -0400

    Revert "Tercer commit: Cambiar formato del mensaje de salida"
    This reverts commit 29df6d7675d8ac347f44daaa0c25f0782d2ab7ac.

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def roll(amount:int, sides:int):
     return dice.roll(f'{amount}d{sides}')
 
 for idx, result in enumerate(roll(5,6)):
-    print(f'Lanzamiento {idx+1} resultado obtenido {result}')
+    print(f'Lanzamiento {idx+1} número obtenido {result}')
     sleep(5)
